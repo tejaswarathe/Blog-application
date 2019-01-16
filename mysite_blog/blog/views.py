62fc94859cba3e89bbb24b14285852cc9262ec45
tejaswarathe <tejaswarathe@hotmail.com>
2019-01-16 18:24:34 +0530

    bug correction

--- mysite_blog/blog/views.py
+++ mysite_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
-def Post_list(request):
+def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts':posts})
 
